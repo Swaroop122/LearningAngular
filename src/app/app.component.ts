629e7ae00774161efe553613fe89e0726ce79c31
Swaroop Kori <skori@BLR.ALLEGISINDIA.COM>
2021-10-21 16:23:12 +0530

    p2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,35 @@
 import { Component } from '@angular/core';
 import { Employee } from 'src/models/employee';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'p1';
 
   employee:Employee;
 
-  constructor()
+  employeeList:Employee[];
+
+  constructor(private httpClient:HttpClient)
   {
-    this.employee=new Employee(101,'raj','sirsi',10000);
+    console.log('We are in the constructor');
+    
+  }
+   
+  ngOnInit()
+  {
+  console.log('we are in the ngoninit method');
+  
+  this.employee=new Employee(101,'raj','sirsi',10000);
+  this.httpClient.get<Employee[]>(https://localhost:44340/api/Employee').subscribe
+    (list=>
+      {
+        this.employeeList=list;
+      }
+    );
   }
 }
